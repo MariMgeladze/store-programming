d83d0aa3d83488d4fe57631a2b60952693e431aa
MariMgeladze <mgeladze17@gmail.com>
2023-10-02 10:24:12 +0400

    Softlab homework

---
+++ src/main/java/softlab/homework/storeprogramming/StoreProgrammingApplication.java
@@ -0,0 +1,13 @@
+package softlab.homework.storeprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProgrammingApplication.class, args);
+	}
+
+}
